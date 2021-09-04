d996400dee53391e525b198aff39cac30ac27b04
sunsetime <sunsetdawn@163.com>
2021-09-04 11:52:46 +0800

    添加项目文件。

---
+++ UI/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace UI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnFile = new System.Windows.Forms.Button();
+            this.Labe = new System.Windows.Forms.Label();
+            this.BtnS = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnFile
+            // 
+            this.BtnFile.BackColor = System.Drawing.SystemColors.Control;
+            this.BtnFile.Location = new System.Drawing.Point(141, 247);
+            this.BtnFile.Name = "BtnFile";
+            this.BtnFile.Size = new System.Drawing.Size(97, 23);
+            this.BtnFile.TabIndex = 3;
+            this.BtnFile.Text = "选择";
+            this.BtnFile.UseVisualStyleBackColor = false;
+            this.BtnFile.Click += new System.EventHandler(this.BtnFile_Click);
+            // 
+            // Labe
+            // 
+            this.Labe.AutoSize = true;
+            this.Labe.BackColor = System.Drawing.Color.Transparent;
+            this.Labe.Font = new System.Drawing.Font("宋体", 17.83486F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Labe.Location = new System.Drawing.Point(93, 141);
+            this.Labe.Name = "Labe";
+            this.Labe.Size = new System.Drawing.Size(72, 27);
+            this.Labe.TabIndex = 4;
+            this.Labe.Text = "----";
+            this.Labe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnS
+            // 
+            this.BtnS.BackColor = System.Drawing.SystemColors.Control;
+            this.BtnS.Location = new System.Drawing.Point(30, 247);
+            this.BtnS.Name = "BtnS";
+            this.BtnS.Size = new System.Drawing.Size(105, 23);
+            this.BtnS.TabIndex = 5;
+            this.BtnS.Text = "开始";
+            this.BtnS.UseVisualStyleBackColor = false;
+            this.BtnS.Click += new System.EventHandler(this.BtnS_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar1.Location = new System.Drawing.Point(30, 288);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(208, 19);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.CausesValidation = false;
+            this.tbFilePath.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tbFilePath.Enabled = false;
+            this.tbFilePath.Location = new System.Drawing.Point(12, 218);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.ReadOnly = true;
+            this.tbFilePath.Size = new System.Drawing.Size(239, 23);
+            this.tbFilePath.TabIndex = 7;
+            this.tbFilePath.Text = "未选择文件";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UI.Properties.Resources.imgbin_borders_and_frames_frame_decorative_arts_png;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(263, 383);
+            this.Controls.Add(this.tbFilePath);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.BtnS);
+            this.Controls.Add(this.Labe);
+            this.Controls.Add(this.BtnFile);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CYRoll";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnFile;
+        private System.Windows.Forms.Label Labe;
+        private System.Windows.Forms.Button BtnS;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox tbFilePath;
+    }
+}
+
---
+++ UI/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace UI
+{
+    public partial class MainForm : Form
+    {
+
+        List<string> nameList = new List<string>();
+        Thread rd = null;
+        string type = "";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFile_Click(object sender, EventArgs e)
+        {
+
+            BtnS.Text = "开始";
+            progressBar1.Value = 0;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string filePath = ofd.FileName;
+            tbFilePath.Text = filePath;
+            if (filePath != "")
+            {
+                Thread.Sleep(1000);
+                progressBar1.Value += 100;
+
+                StreamReader sr = new StreamReader(filePath, UnicodeEncoding.GetEncoding("UTF-8"));
+
+                string str = "";
+                while ((str = sr.ReadLine()) != null)
+                {
+                    nameList.Add(str);
+                }
+            }
+            else 
+            {
+                MessageBox.Show("未选择文件", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void BtnS_Click(object sender, EventArgs e)
+        {
+            if(progressBar1.Value==100)
+            {
+            rd = new Thread(new ThreadStart(Random));
+            if (BtnS.Text == "开始")
+            {
+                type = "start";
+                BtnS.Text = "停止";
+                rd.Start();
+
+            }
+            else
+            {
+                BtnS.Text = "开始";
+                type = "stop";
+                rd.Abort();
+            }
+            }
+        }
+
+        public void Random()
+        {
+            while(BtnS.Text!="开始")
+                {
+                foreach (string name in nameList)
+                {
+                    if(type=="stop")
+                        {
+                            break;
+                        }
+                
+                    if (Labe.InvokeRequired)
+                    {
+                        // 当一个控件的InvokeRequired属性值为真时，说明有一个创建它以外的线程想访问它
+                        Action<string> actionDelegate = (x) => { this.Labe.Text = x.ToString(); };
+                        
+                        this.Labe.Invoke(actionDelegate, name);
+                        Thread.Sleep(50);
+                    }
+                    
+                }
+          
+            }
+
+        }
+
+    }
+}
